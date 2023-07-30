e38bddefc60a77a1a7c8742608a66a4c08505c13
jthw1005 <skl0105@naver.com>
2023-07-30 16:02:20 +0900

    design: update header UI

--- 1-producing-chart/src/components/Header.jsx
+++ 1-producing-chart/src/components/Header.jsx
@@ -1,5 +1,19 @@
+import styled from 'styled-components';
+
+const HEADER_HEIGHT = 150;
+
 const Header = () => {
-  return <div>header</div>;
+  return <HeaderBox>Game Score Chart</HeaderBox>;
 };
+
+const HeaderBox = styled.div`
+  width: 100%;
+  height: ${HEADER_HEIGHT}px;
+  text-align: center;
+  line-height: ${HEADER_HEIGHT}px;
+  font-size: 60px;
+  border: 1px solid black;
+  border-radius: 20px;
+`;
 
 export default Header;
