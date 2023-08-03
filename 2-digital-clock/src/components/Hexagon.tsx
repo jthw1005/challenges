3ff89ab13d1e69f4932cf6acf8a6aaf4ee7d115d
jthw1005 <skl0105@naver.com>
2023-08-03 23:53:30 +0900

    feat: make Hexagon component

---
+++ 2-digital-clock/src/components/Hexagon.tsx
@@ -0,0 +1,53 @@
+import { styled } from 'styled-components';
+
+interface IHexagonProps {
+  width: number;
+  height: number;
+  top: number;
+  left: number;
+  isOn: boolean;
+  rotate?: boolean;
+}
+
+interface IHexagonWrapperProps {
+  width: number;
+  top: number;
+  left: number;
+  rotate?: boolean;
+}
+
+interface IHexagonContentProps {
+  width: number;
+  height: number;
+  isOn: boolean;
+}
+
+const Hexagon = ({ width, height, top, left, rotate, isOn }: IHexagonProps) => (
+  <HexagonWrapper width={width} top={top} left={left} rotate={rotate}>
+    <HexagonContent width={width} height={height} isOn={isOn} />
+  </HexagonWrapper>
+);
+
+const HexagonWrapper = styled.div<IHexagonWrapperProps>`
+  position: absolute;
+  width: ${(props) => props.width}px;
+  top: ${(props) => props.top}px;
+  left: ${(props) => props.left}px;
+  background-color: transparent;
+  transform: ${({ rotate }) => (rotate ? 'rotate(90deg)' : 'rotate(0deg)')};
+`;
+
+const HexagonContent = styled.div<IHexagonContentProps>`
+  position: absolute;
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  clip-path: ${({ width, height }) =>
+    `polygon(${Math.round((height * 50) / width)}% 0%, ${Math.round(
+      100 - (height * 50) / width
+    )}% 0%, 100% 50%, ${Math.round(
+      100 - (height * 50) / width
+    )}% 100%, ${Math.round((height * 50) / width)}% 100%, 0% 50%)`};
+  background-color: ${({ isOn }) => (isOn ? '#00F604' : '#fff')};
+`;
+
+export default Hexagon;
