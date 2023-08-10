8b3b464a36b8f95f03012eedf4d09eb089e16250
jthw1005 <skl0105@naver.com>
2023-08-10 23:52:43 +0900

    feat: 사진 추가 기능 구현

--- 4-presentation/presentation/src/App.tsx
+++ 4-presentation/presentation/src/App.tsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import Layout from './components/Layout';
-import Router from './router';
+
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Main from './pages/Main';
+
+const INIT_VALUE = 0;
+const LIST_SIZE = 5;
 
 const App = () => {
+  const [images, setImages] = useState<string[]>([]);
+  const [currIdx, setCurrIdx] = useState<number>(INIT_VALUE);
+  const [startIdx, setStartIdx] = useState<number>(INIT_VALUE);
+  const [endIdx, setEndIdx] = useState<number>(INIT_VALUE + LIST_SIZE);
+
   return (
     <Layout>
-      <Router />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home setImages={setImages} />} />
+          <Route path="main" element={<Main />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </Layout>
   );
 };
--- 4-presentation/presentation/src/pages/Home.tsx
+++ 4-presentation/presentation/src/pages/Home.tsx
@@ -1,14 +1,45 @@
+import { ChangeEvent, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
 
-const Home = () => {
-  const handleButtonClick = () => {
-    console.log('clicked');
+interface IHomeProps {
+  setImages: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const Home = ({ setImages }: IHomeProps) => {
+  const navigate = useNavigate();
+
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+
+    if (!files) {
+      return;
+    }
+
+    for (let i = 0; i < files.length; i++) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImages((prevImages: string[]) => [
+          ...prevImages,
+          reader.result as string,
+        ]);
+        navigate('/main');
+      };
+      reader.readAsDataURL(files[i]);
+    }
   };
 
   return (
     <HomeBox>
       <Title>4th challenge - Presentation</Title>
-      <Button onClick={handleButtonClick}>Add pictures</Button>
+      <Button htmlFor="image-upload">Add pictures</Button>
+      <ImageInput
+        id="image-upload"
+        type="file"
+        accept="image/png, image/jpeg"
+        multiple
+        onChange={handleImageChange}
+      />
     </HomeBox>
   );
 };
@@ -24,12 +55,19 @@ const Title = styled.h1`
   font-size: 40px;
 `;
 
-const Button = styled.button`
+const ImageInput = styled.input`
+  display: none;
+`;
+
+const Button = styled.label`
   width: 200px;
   height: 150px;
+  text-align: center;
+  line-height: 150px;
   font-size: 20px;
   background-color: skyblue;
   border-radius: 20px;
+  border: 2px solid black;
   cursor: pointer;
 `;
 
