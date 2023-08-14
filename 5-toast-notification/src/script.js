24ddb553079000c4c852f3718726a5f861d210eb
jthw1005 <skl0105@naver.com>
2023-08-14 18:26:37 +0900

    feat: initToast 구현

--- 5-toast-notification/src/script.js
+++ 5-toast-notification/src/script.js
@@ -1,1 +1,13 @@
-// write your JavaScript here
+const initToast = () => {
+  const $toastContainer = document.getElementById('toast');
+  const toastPositions = ['left', 'right'];
+
+  toastPositions.forEach((toastPosition) => {
+    const $ = document.createElement('div');
+    $.id = `toast-${toastPosition}`;
+    $toastContainer.appendChild($);
+  });
+};
+
+initToast();
+
